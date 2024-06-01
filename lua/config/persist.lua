29c7338fa7326b7e7f913cc9bc87a0d3cfb7ac1a
Mariano Heller <mariano.heller@personio.de>
2024-06-01 23:55:20 +0200

    "automatic-2024-06-01T21:55:20Z"

--- lua/config/persist.lua
+++ lua/config/persist.lua
@@ -2,8 +2,10 @@ vim.api.nvim_create_autocmd('User', {
     pattern = 'VeryLazy',
     callback = function()
         local cfg_path = get_config_path()
+        local commit_msg = "\"automatic-" .. get_timestamp() .. "\""
+
         vim.system({ "git", "add", "." }, { cwd = cfg_path })
-        vim.system({ "git", "commit", "-m", "\"asdasd\"" }, { cwd = cfg_path })
+        vim.system({ "git", "commit", "-m", commit_msg }, { cwd = cfg_path })
         vim.system({ "git", "push" }, { cwd = cfg_path })
     end
 })
@@ -16,4 +18,8 @@ function get_config_path()
     else
         return cfg_path
     end
+end
+
+function get_timestamp()
+    return os.date("!%Y-%m-%dT%TZ")
 end
